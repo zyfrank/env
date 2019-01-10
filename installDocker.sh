2e00396ace9e5712ad88b102815c69cfe173a396
zyfrank <zyfrank@hotmail.com>
2019-01-10 15:53:40 +0800

    change bash env

--- installDocker.sh
+++ installDocker.sh
@@ -5,4 +5,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -echo
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install  docker-ce
+sudo groupadd docker
+sudo gpasswd -a ${USER} docker
 sudo cp ./daemon.json  /etc/docker/
+sudo shutdown -r now
